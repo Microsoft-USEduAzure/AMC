f2f3a3fc81400fb9d480b3815b050c3ca3aae830
kfprugger <jbrake30@gmail.com>
2019-10-10 09:26:52 -0400

    update templates

--- amc-rwb/rwb-deploy.ps1
+++ amc-rwb/rwb-deploy.ps1
@@ -4,8 +4,21 @@
 
 $rwbname = "rwb"
 $location = "eastus"
+$rwbrg = "$rwbname-rg"
+$rwbmgmtrg = "$rwbname"+ "mgmt-rg"
 
-New-AzResourceGroup -Name "$rwbname1-rg" -Location $location
 
-# Deploy the foundational VNET
-New-AzureRmResourceGroupDeployment -TemplateParameterFile ".\0-foundation\parameters.json" -TemplateFile ".\0-foundation\template.json"
+
+# Deploy the foundational VNET If not deploying the Bastion Host
+#New-AzResourceGroupDeployment -TemplateParameterFile ".\0-foundation\parameters.json" -TemplateFile ".\0-foundation\template.json" -ResourceGroupName $rwbmgmtrg
+
+# Deploy Resource Group for VNET and Bastion Host
+New-AzResourceGroup -name $rwbmgmtrg -Location $location
+# Deploy the Bastion Host for Secured VNET access to all your IaaS resources
+New-AzResourceGroupDeployment -TemplateFile ".\1-azbastionbroker\bastiondeploy.json" -ResourceGroupName $rwbmgmtrg -TemplateParameterFile ".\1-azbastionbroker\bastiondeploy.parameters.json" -location $location 
+
+# Now Let's Deploy the Workbench Resources
+New-AzResourceGroup -name $rwbrg -Location $location
+
+# First let's deploy a secure Ubuntu Data Science VM
+New-AzResourceGroupDeployment -TemplateFile ".\2-dsvm-ubuntu\udsvm.template.json" -ResourceGroupName $rwbmgmtrg -TemplateParameterFile ".\2-dsvm-ubuntu\udsvm.parameters.json" -location $location 
