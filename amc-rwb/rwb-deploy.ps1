ed786e6256483d028e55af0c6f4fe28a7acccd4a
kfprugger <jbrake30@gmail.com>
2019-10-15 15:45:37 -0400

    update posh

--- amc-rwb/rwb-deploy.ps1
+++ amc-rwb/rwb-deploy.ps1
@@ -10,21 +10,39 @@ $rwbmgmtrg = "$rwbname"+ "mgmt-rg"
 # Deploy the foundational VNET for IaaS Workloads
 New-AzResourceGroup -name $rwbmgmtrg -Location $location
 $vnetparams = @{
-    vnetName = @("rwb-vnet")
+    vnetName = "$rwbname-vnet"
 }
 $vnetdeploy = New-AzResourceGroupDeployment -TemplateParameterObject $vnetparams -TemplateFile ".\0-foundation\rwbvnet.template.json" -ResourceGroupName $rwbmgmtrg
 
-$vnet = Get-AzVirtualNetwork -ResourceGroupName $rwbmgmtrg -Name ($vnetdeploy.ParametersString | ? {$_.name -eq "vnetName"})
+$vnet = Get-AzVirtualNetwork -ResourceGroupName $rwbmgmtrg -Name "$rwbname-vnet"
+
 # Deploy Resource Group for VNET and Bastion Host
 New-AzResourceGroup -name $rwbmgmtrg -Location $location
 # Deploy the Bastion Host for Secured VNET access to all your IaaS resources
-New-AzResourceGroupDeployment -TemplateFile ".\1-azbastionbroker\bastiondeploy.json" -ResourceGroupName $rwbmgmtrg -TemplateParameterFile ".\1-azbastionbroker\bastiondeploy.parameters.json" -location $location 
+$bastionparams = @{
+    location = $location
+    "vnet-name" = $vnet.name
+    "vnet-ip-prefix" = "10.5.0.0/16"
+    "vnet-new-or-existing" = "existing"
+    "bastion-subnet-ip-prefix" = "10.5.0.0/27"
+    "bastion-host-name" = "$rwbname-bastion"
 
-# Deploy the foundational VNET for IaaS Workloads
-New-AzResourceGroupDeployment -TemplateParameterFile ".\0-foundation\rwbvnet.parameters.json" -TemplateFile ".\0-foundation\rwbvnet.template.json" -ResourceGroupName $rwbmgmtrg
+}
+New-AzResourceGroupDeployment -TemplateFile ".\1-azbastionbroker\bastiondeploy.json" -ResourceGroupName $rwbmgmtrg -TemplateParameterObject $bastionparams -location $location 
+
 
 # Now Let's Deploy the Workbench Resources
 New-AzResourceGroup -name $rwbrg -Location $location
 
 # First let's deploy a secure Ubuntu Data Science VM
-New-AzResourceGroupDeployment -TemplateFile ".\2-dsvm-ubuntu\udsvm.template.json" -ResourceGroupName $rwbmgmtrg -TemplateParameterFile ".\2-dsvm-ubuntu\udsvm.parameters.json" -location $location 
+## Let's replace the VNET ID with the one you deployed above
+$udsvmpfilepath = ".\2-dsvm-ubuntu\udsvm.parameters.json"
+$udsvmparams = Get-Content -Path $udsvmpfilepath -Raw | ConvertFrom-Json
+$udsvmparams.parameters.virtualNetworkId = @{value=$vnet.id}  
+$udsvmparams | ConvertTo-Json -Depth 100 | foreach { [System.Text.RegularExpressions.Regex]::Unescape($_) } | Set-Content -Path '.\custom-udsvm.parameters.json'
+
+
+## Now deploy the DSVM to Azure
+New-AzResourceGroupDeployment -TemplateFile ".\2-dsvm-ubuntu\udsvm.template.json" -ResourceGroupName $rwbrg -TemplateParameterFile '.\custom-udsvm.parameters.json' -location $location 
+
+
