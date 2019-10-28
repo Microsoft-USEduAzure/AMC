cbff047ce74c7a355718527bdd050a10dcf1946a
kfprugger <jbrake30@gmail.com>
2019-10-28 16:43:33 -0400

    update bash

---
+++ amc-rwb/bashinstall.sh
@@ -0,0 +1,8 @@
+# Install Powershell Core
+
+## First, install the PoSH Install
+## Follow the MS Doc: 
+## https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux?view=powershell-6#debian-10
+## Then install the following:
+sudo apt-get update
+sudo apt-get install libunwind8 icu-devtools
--- amc-rwb/rwb-deploy.ps1
+++ amc-rwb/rwb-deploy.ps1
@@ -40,12 +40,6 @@ sudo apt-get install wget
 
 
 
-# Install Powershell Core
-## Follow the MS Doc: 
-## https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux?view=powershell-6#debian-10
-## Then install the following:
-sudo apt-get update
-sudo apt-get install libunwind8 icu-devtools
 
 # Now Let's Deploy the Workbench Resources
 New-AzResourceGroup -name $rwbrg -Location $location
