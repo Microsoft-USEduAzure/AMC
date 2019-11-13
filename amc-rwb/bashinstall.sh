1fd7de5987e2a859f0fbb3496b911eed094eb9e2
kfprugger <jbrake30@gmail.com>
2019-11-13 12:24:50 -0500

    commit changes for AMC
    mdw_deploy-rwb 0.1

--- amc-rwb/bashinstall.sh
+++ amc-rwb/bashinstall.sh
@@ -1,5 +1,14 @@
 # Install Powershell Core
 
+# Working from local WSL/Debian Installation?
+## If working from WSL Debian (Windows Store), use these instructions
+
+sudo apt-get update
+sudo apt-get install curl
+sudo apt-get install wget
+sudo apt-get install unzip
+
+
 ## First, install the PoSH Install
 ## Follow the MS Doc: 
 ## https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux?view=powershell-6#debian-10
